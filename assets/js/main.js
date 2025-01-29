45a72c4dd628096852e75415d9828bb5cfa3a5f2
PETIT Valentin <vxlleneydevelopment@gmail.com>
2025-01-29 13:44:30 +0100

    Mise en git du portfolio

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+// NAVBAR RESPONSIVE 
+const btnNavMenu = document.querySelectorAll('#btn-nav-menu');
+const navMenu    = document.querySelector('.nav-menu');
+
+btnNavMenu.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        navMenu.classList.toggle('hide');
+    });
+});
+
+
+// CHANGE THEME
+
+const fileSwitchTheme = document.querySelector('#file-theme');
+applyTheme(detectTheme())
+
+function detectTheme() {
+    return (localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light');
+    // Si le localstorage Theme existe, on le return, sinon => Light
+}
+
+function switchTheme() {
+
+    if (detectTheme() == 'dark') {
+        localStorage.setItem('theme', 'light');
+    } else {
+        localStorage.setItem('theme', 'dark');
+    }
+    applyTheme()
+}
+
+function applyTheme() {
+
+    if (detectTheme() == 'dark') {
+        fileSwitchTheme.href = "assets/css/styles_dark.css";
+    } else {
+        fileSwitchTheme.href = "assets/css/styles.css";
+    }
+}
+
+function toggleTheme() {
+    switchTheme()
+}
